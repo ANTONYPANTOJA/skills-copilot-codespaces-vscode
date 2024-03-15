b072b92ec97162f38ebf308486e8f1027b46d1e8
ANTONYPANTOJA <94948196+ANTONYPANTOJA@users.noreply.github.com>
2024-03-15 15:07:01 +0000

    Copilot third  commit

---
+++ coments.js
@@ -0,0 +1,103 @@
+// create web server
+var express = require('express');
+var app = express();
+
+// load the file system module
+var fs = require('fs');
+
+// load the database connection
+var mysql = require('mysql');
+
+// load the body-parser
+var bodyParser = require('body-parser');
+
+// load the express-session
+var session = require('express-session');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+// load the express-flash
+var flash = require('express-flash');
+
+// load the express-validator
+var expressValidator = require('express-validator');
+
+// load the express-fileupload
+var fileUpload = require('express-fileupload');
+
+//
+
+
